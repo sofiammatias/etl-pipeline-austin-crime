72e2a8b3b4ce115c8e1c3966645bc47a410b8553
Sofia Matias <114782592+sofiammatias@users.noreply.github.com>
2024-06-10 17:36:16 +0100

    added if there's data for See_The_Data page.

--- pages/3_See_The_Dashboard.py
+++ pages/3_See_The_Dashboard.py
@@ -51,39 +51,42 @@ destination_path = f"{dest_folder}/{dataset_id}.json"
 # Building app
 st.title("ETL Pipeline - Austin Crime Database 👮‍♂️")
 
-engine = create_engine(f'postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}')
+if finished_workflow == 'false':
+    st.info ("There's no data to be seen. Run 'Start Pipeline' button first, in 'See The Pipeline'")
+else:
+    engine = create_engine(f'postgresql://{postgres_user}:{postgres_password}@{postgres_host}:{postgres_port}/{postgres_database}')
 
-df_crimes, df_geo, df_hour, df_year, df_top_crimes = read_all_tables_from_postgres(table_id)
-df_top_crimes = df_top_crimes.sort_values('number_of_crimes', ascending=False)[:10]
+    df_crimes, df_geo, df_hour, df_year, df_top_crimes = read_all_tables_from_postgres(table_id)
+    df_top_crimes = df_top_crimes.sort_values('number_of_crimes', ascending=False)[:10]
 
-col1, col2 = st.columns(2)
-fontsize=5
+    col1, col2 = st.columns(2)
+    fontsize=5
 
-with col1:
-    st.subheader('Number of Crimes by Hour of Day')
-    st.bar_chart (df_hour, x="hour", y="number_of_crimes")
-    fig1, ax1 = plt.subplots()  
-    labels = df_top_crimes['crime_type'].values.tolist()
-    ax1.pie (df_top_crimes['number_of_crimes'].values.tolist())
-    fig1.patch.set_facecolor("#0E1117")
-    fig1.legend(labels=labels, fontsize=fontsize)
-    st.subheader('Number of Crimes by Crime Type (Top10)')
-    st.pyplot(fig1) 
-
-
-with col2:
-    st.subheader('Number of Crimes per Year')
-    st.bar_chart (df_year, x="year", y="number_of_crimes", color=['#cf3251'])
-
-    df_location = df_crimes[['location_type']].value_counts('location_type', sort=True, ascending=False)[:10]
-    fig2, ax2 = plt.subplots()  
-    labels = df_location.index.tolist()
-    ax2.pie (df_location.values.tolist())
-    fig2.patch.set_facecolor("#0E1117")
-    fig2.legend(labels=labels, fontsize=fontsize)
-    st.subheader('Number of Crimes by Location Type (Top10)')
-    st.pyplot(fig2) 
+    with col1:
+        st.subheader('Number of Crimes by Hour of Day')
+        st.bar_chart (df_hour, x="hour", y="number_of_crimes")
+        fig1, ax1 = plt.subplots()  
+        labels = df_top_crimes['crime_type'].values.tolist()
+        ax1.pie (df_top_crimes['number_of_crimes'].values.tolist())
+        fig1.patch.set_facecolor("#0E1117")
+        fig1.legend(labels=labels, fontsize=fontsize)
+        st.subheader('Number of Crimes by Crime Type (Top10)')
+        st.pyplot(fig1) 
 
 
-st.subheader('Crime Density Within Austin City')
-st.map(df_geo)
+    with col2:
+        st.subheader('Number of Crimes per Year')
+        st.bar_chart (df_year, x="year", y="number_of_crimes", color=['#cf3251'])
+
+        df_location = df_crimes[['location_type']].value_counts('location_type', sort=True, ascending=False)[:10]
+        fig2, ax2 = plt.subplots()  
+        labels = df_location.index.tolist()
+        ax2.pie (df_location.values.tolist())
+        fig2.patch.set_facecolor("#0E1117")
+        fig2.legend(labels=labels, fontsize=fontsize)
+        st.subheader('Number of Crimes by Location Type (Top10)')
+        st.pyplot(fig2) 
+
+
+    st.subheader('Crime Density Within Austin City')
+    st.map(df_geo)
